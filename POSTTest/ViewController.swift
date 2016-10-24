21efabfa138cf7782920d08ce432d4859d7a3f6b
ComarPers 922 <apple@ComarPers922s-Apple-MacBook-Air.local>
2016-10-24 16:41:16 -0400

    I am ready to share\!

--- POSTTest/ViewController.swift
+++ POSTTest/ViewController.swift
@@ -8,14 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var info: UILabel!
+    
+    @IBOutlet weak var num1: UITextField!
+    
+    @IBOutlet weak var num2: UITextField!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    @IBAction func PostIt(_ sender: AnyObject)
+    {
+        let request = NSMutableURLRequest(url: NSURL(string: "http://127.0.0.1:4700")! as URL)
+        request.httpMethod = "POST"
+        request.httpBody = "num1=\(num1.text!)&num2=\(num2.text!)".data(using: String.Encoding.utf8)
+        let task = URLSession.shared.dataTask(with: request as URLRequest)
+        {
+            (data, response, error) in
+            if let _ = error
+            {
+                DispatchQueue.main.async
+                {
+                        self.info.text = "\(error)"
+                }
+            }
+            else
+            {
+                DispatchQueue.main.async
+                {
+                    self.info.text = String(data: data!, encoding: String.Encoding.utf8)
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
